e988376628bf49c23d0efd84d639f612db64cc0c
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2015-12-29 19:32:33 +0000

    Implement basic RTM send and receive

--- aslack/slack_bot.py
+++ aslack/slack_bot.py
@@ -1,9 +1,13 @@
 """A Slack bot using the real-time messaging API."""
 
-import logging
+from itertools import count
 import json
+import logging
+from random import randint
+from textwrap import dedent
 
 import aiohttp
+from aiohttp import MsgType
 
 from .slack_api import SlackApiError, SlackBotApi
 
@@ -15,6 +19,20 @@ class SlackBot:
 
     API_AUTH_ENDPOINT = 'auth.test'
     """Test endpoint for API authorisation."""
+
+    MESSAGE_FILTERS = {}
+    """Default filters for incoming messages."""
+
+    INSTRUCTIONS = dedent("""
+    These are the default instructions for an aSlack bot ({aslack_url}).
+
+    Override these as appropriate for your specific needs.
+
+    Hassle <@{owner_id}> if you need more help or information.
+    """).format(
+        aslack_url='https://github.com/textbook/aslack',
+        owner_id='U0EVC55NX',
+    )
 
     RTM_HANDSHAKE = {'type': 'hello'}
     """Expected handshake message from RTM API."""
@@ -34,12 +52,105 @@ class SlackBot:
         self.id_ = id_
         self.user = user
         self.api = api
+        self.address_as = '<@{}>: '.format(id_)
+        self._msg_ids = count(randint(1, 1000))
 
-    async def join_rtm(self):
-        """Join the real-time messaging service."""
-        async with aiohttp.ws_connect(await self._get_socket_url()) as socket:
+    async def join_rtm(self, filters=None):
+        """Join the real-time messaging service.
+
+        Arguments:
+          filters (dict, optional): Dictionary mapping message filters
+            to the functions they should dispatch to. Use an
+            OrderedDict if precedence is important; only one filter,
+            the first match, will be applied to each message.
+
+        """
+        if filters is None:
+            filters = self.MESSAGE_FILTERS
+        url = await self._get_socket_url()
+        logging.debug('Connecting to {!r}'.format(url))
+        async with aiohttp.ws_connect(url) as socket:
             first_msg = await socket.receive()
             self._validate_first_message(first_msg)
+            async for message in socket:
+                if message.tp == aiohttp.MsgType.text:
+                    result = self._handle_message(message, filters)
+                    if result is not None:
+                        socket.send_str(result)
+                elif message.tp in (MsgType.closed, MsgType.error):
+                    if not socket.closed:
+                        await socket.close()
+                    break
+        logger.info('Left real-time messaging.')
+
+    @classmethod
+    async def from_api_token(cls, token):
+        """Create a new instance from the API token.
+
+        Arguments:
+          token (str): The bot's API token.
+
+        Returns:
+          SlackBot: The new instance.
+
+        """
+        api = SlackBotApi(token)
+        data = await api.execute_method(cls.API_AUTH_ENDPOINT)
+        return cls(data['user_id'], data['user'], api)
+
+    def _format_message(self, channel, text):
+        """Format an outoging message for transmission.
+
+        Notes:
+          Adds the message type ('message') and incremental ID.
+
+        Arguments:
+          channel (str): The channel to send to.
+          text (str): The message text to send.
+
+        Returns:
+          str: The JSON string of the message.
+
+        """
+        payload = {'type': 'message', 'id': next(self._msg_ids)}
+        payload.update(channel=channel, text=text)
+        return json.dumps(payload)
+
+    async def _get_socket_url(self):
+        """Get the WebSocket URL for the RTM session.
+
+        Notes:
+          The URL expires if the session is not joined within 30
+          seconds of the API call to the start endpoint.
+
+        Returns:
+          str: The socket URL.
+
+        """
+        data = await self.api.execute_method(self.RTM_START_ENDPOINT)
+        return data['url']
+
+    def _handle_message(self, message, filters):
+        """Handle an incoming message appropriately.
+
+        Arguments:
+          message (aiohttp.Message): The incoming message to handle.
+          filters (dict): The filters to apply to incoming messages.
+
+        Returns:
+          str: The response to send as a result.
+
+        """
+        data = self._unpack_message(message)
+        logger.debug(data)
+        if data.get('type') == 'error':
+            raise SlackApiError(
+                data.get('error', {}).get('msg', str(data))
+            )
+        for filter_, dispatch in filters.items():
+            if filter_(data):
+                logger.debug('Response triggered')
+                return self._format_message(**dispatch(data))
 
     @classmethod
     def _validate_first_message(cls, msg):
@@ -74,32 +185,3 @@ class SlackBot:
 
         """
         return json.loads(msg.data)
-
-    async def _get_socket_url(self):
-        """Get the WebSocket URL for the RTM session.
-
-        Notes:
-          The URL expires if the session is not joined within 30
-          seconds of the API call to the start endpoint.
-
-        Returns:
-          str: The socket URL.
-
-        """
-        data = await self.api.execute_method(self.RTM_START_ENDPOINT)
-        return data['url']
-
-    @classmethod
-    async def from_api_token(cls, token):
-        """Create a new instance from the API token.
-
-        Arguments:
-          token (str): The bot's API token.
-
-        Returns:
-          SlackBot: The new instance.
-
-        """
-        api = SlackBotApi(token)
-        data = await api.execute_method(cls.API_AUTH_ENDPOINT)
-        return cls(data['user_id'], data['user'], api)
--- tests/test_slack_bot.py
+++ tests/test_slack_bot.py
@@ -7,11 +7,15 @@ from aslack.slack_api import SlackApiError, SlackBotApi
 from aslack.slack_bot import SlackBot
 
 
-def test_init():
+@mock.patch('aslack.slack_bot.randint', return_value=10)
+def test_init(randint):
     args = dict(id_='foo', user='bar', api='baz')
     bot = SlackBot(**args)
     for attr, val in args.items():
         assert getattr(bot, attr) == val
+    assert bot.address_as == '<@foo>: '
+    assert next(bot._msg_ids) == randint.return_value
+    randint.assert_called_once_with(1, 1000)
 
 
 @mock.patch(
@@ -61,11 +65,52 @@ def test_unpack_message_broken_json():
     ('{"foo": "bar"}', SlackApiError),
     ('broken.json', json.JSONDecodeError),
 ])
-@mock.patch('aslack.slack_bot.logging')
-def test_validate_first_message(logging, data, raises):
+def test_validate_first_message(data, raises):
     mock_message = mock.Mock(data=data)
     if raises is None:
         assert SlackBot._validate_first_message(mock_message) is None
     else:
         with pytest.raises(raises):
             SlackBot._validate_first_message(mock_message)
+
+
+@mock.patch('aslack.slack_bot.randint', return_value=10)
+def test_format_message(randint):
+    bot = SlackBot(None, None, None)
+    expected = dict(
+        id=randint.return_value,
+        channel='foo',
+        text='bar',
+        type='message',
+    )
+    assert json.loads(bot._format_message('foo', 'bar')) == expected
+
+
+@mock.patch('aslack.slack_bot.randint', return_value=10)
+def test_handle_message(randint):
+    bot = SlackBot(None, None, None)
+    mock_message = mock.Mock(data='{}')
+    mock_filter_ = mock.Mock(return_value=True)
+    mock_dispatch = mock.Mock(return_value=dict(channel='foo', text='bar'))
+    response = bot._handle_message(mock_message, {mock_filter_: mock_dispatch})
+    expected = dict(
+        id=randint.return_value,
+        type='message',
+        **mock_dispatch.return_value,
+    )
+    assert json.loads(response) == expected
+    mock_filter_.assert_called_once_with({})
+    mock_dispatch.assert_called_once_with({})
+
+
+def test_handle_error_message():
+    bot = SlackBot(None, None, None)
+    mock_msg = mock.Mock(data=json.dumps(dict(error={}, type='error')))
+    with pytest.raises(SlackApiError):
+        bot._handle_message(mock_msg, {})
+
+
+def test_handle_unfiltered_message():
+    bot = SlackBot(None, None, None)
+    mock_msg = mock.Mock(data=json.dumps(dict(type='message')))
+    bot._handle_message(mock_msg, {lambda msg: False: None})
