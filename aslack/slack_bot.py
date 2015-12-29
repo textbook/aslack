49c3a5541863a839a9abfdc12bb14a5174d03097
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2015-12-29 11:51:28 +0000

    Start implementing RTM functionality
    Bot can now connect to the websocket and validate that the first message matches the expected handshake

--- aslack/slack_bot.py
+++ aslack/slack_bot.py
@@ -1,8 +1,11 @@
 """A Slack bot using the real-time messaging API."""
 
 import logging
+import json
 
-from .slack_api import SlackBotApi
+import aiohttp
+
+from .slack_api import SlackApiError, SlackBotApi
 
 logger = logging.getLogger(__name__)
 
@@ -12,6 +15,9 @@ class SlackBot:
 
     API_AUTH_ENDPOINT = 'auth.test'
     """Test endpoint for API authorisation."""
+
+    RTM_HANDSHAKE = {'type': 'hello'}
+    """Expected handshake message from RTM API."""
 
     RTM_START_ENDPOINT = 'rtm.start'
     """Start endpoint for real-time messaging."""
@@ -28,6 +34,56 @@ class SlackBot:
         self.id_ = id_
         self.user = user
         self.api = api
+
+    async def join_rtm(self):
+        """Join the real-time messaging service."""
+        async with aiohttp.ws_connect(await self._get_socket_url()) as socket:
+            first_msg = await socket.receive()
+            self._validate_first_message(first_msg)
+
+    @classmethod
+    def _validate_first_message(cls, msg):
+        """Check the first message matches the expected handshake.
+
+        Arguments:
+          msg (aiohttp.Message): The message to validate.
+
+        Raises:
+          SlackApiError: If the data doesn't match the handshake.
+
+        """
+        data = cls._unpack_message(msg)
+        logger.debug(data)
+        if data != cls.RTM_HANDSHAKE:
+            raise SlackApiError('Unexpected response: {!r}'.format(data))
+        logger.info('Joined real-time messaging.')
+
+    @staticmethod
+    def _unpack_message(msg):
+        """Unpack the data from the message.
+
+        Arguments:
+          msg (aiohttp.Message): The message to unpack.
+
+        Returns:
+          dict: The loaded data.
+
+        """
+        return json.loads(msg.data)
+
+    async def _get_socket_url(self):
+        """Get the WebSocket URL for the RTM session.
+
+        Notes:
+          The URL expires if the session is not joined within 30
+          seconds of the API call to the start endpoint.
+
+        Returns:
+          str: The socket URL.
+
+        """
+        data = await self.api.execute_method(self.RTM_START_ENDPOINT)
+        return data['url']
 
     @classmethod
     async def from_api_token(cls, token):
--- tests/test_slack_bot.py
+++ tests/test_slack_bot.py
@@ -1,6 +1,9 @@
+import json
+
 from asynctest import mock
 import pytest
 
+from aslack.slack_api import SlackApiError, SlackBotApi
 from aslack.slack_bot import SlackBot
 
 
@@ -22,3 +25,35 @@ async def test_from_api_token(execute_method):
     execute_method.assert_called_once_with('auth.test')
     assert bot.id_ == 'foo'
     assert bot.user == 'bar'
+
+
+@pytest.mark.asyncio
+async def test_get_socket_url():
+    api = mock.CoroutineMock(
+        spec=SlackBotApi,
+        **{'execute_method.return_value': {'url': 'foo'}},
+    )
+    bot = SlackBot(None, None, api)
+    url = await bot._get_socket_url()
+    assert url == 'foo'
+    api.execute_method.assert_called_once_with('rtm.start')
+
+
+def test_unpack_message():
+    data = {'bar': 'foo'}
+    mock_message = mock.Mock(data=json.dumps(data))
+    assert SlackBot._unpack_message(mock_message) == data
+
+
+@pytest.mark.parametrize('data,raises', [
+    ('{}', SlackApiError),
+    ('{"type": "hello"}', None)
+])
+@mock.patch('aslack.slack_bot.logging')
+def test_validate_first_message(logging, data, raises):
+    mock_message = mock.Mock(data=data)
+    if raises is None:
+        assert SlackBot._validate_first_message(mock_message) is None
+    else:
+        with pytest.raises(SlackApiError):
+            SlackBot._validate_first_message(mock_message)
