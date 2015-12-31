454b31dd2b00a67433914ec258fd45796a128308
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2015-12-31 18:17:57 +0000

    Configure launch for Cloud Foundry deploy

---
+++ launch.py
@@ -0,0 +1,32 @@
+import asyncio
+import logging
+from textwrap import dedent
+import sys
+
+from aslack import __author__, slack_bot
+
+logging.basicConfig(
+    datefmt='%Y/%m/%d %H.%M.%S',
+    format='%(levelname)s:%(name)s:%(message)s',
+    level=logging.INFO,
+    stream=sys.stdout,
+)
+
+
+class Halliwell(slack_bot.SlackBot):
+
+    INSTRUCTIONS = dedent("""
+    Hello, I am an aSlack bot running on Cloud Foundry.
+
+    For more information, see {aslack_url} or contact {author}.
+    """.format(
+        aslack_url='https://pythonhosted.org/aslack',
+        author=__author__,
+    ))
+
+
+loop = asyncio.get_event_loop()
+
+bot = loop.run_until_complete(Halliwell.from_api_token())
+
+loop.run_until_complete(bot.join_rtm())
