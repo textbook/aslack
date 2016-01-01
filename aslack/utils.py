314a74f3995b261c2c4bd6668a5fc130b5c85986
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2016-01-01 17:06:10 +0000

    Add function for truncating long text

--- aslack/utils.py
+++ aslack/utils.py
@@ -76,3 +76,23 @@ def get_api_token():
     token = input(template)
     os.environ[API_TOKEN_ENV] = token
     return token
+
+
+def truncate(text, max_len=350, end='...'):
+    """Truncate the supplied text for display.
+
+    Arguments:
+      text (:py:class:`str`): The text to truncate.
+      max_len (:py:class:`int`, optional): The maximum length of the
+        text before truncation (defaults to 350 characters).
+      end (:py:class:`str`, optional): The ending to use to show that
+        the text was truncated (defaults to ``'...'``).
+
+    Returns:
+      :py:class:`str`: The truncated text.
+
+    """
+    if len(text) <= max_len:
+        return text
+    return text[:max_len].rsplit(' ', maxsplit=1)[0] + end
+
