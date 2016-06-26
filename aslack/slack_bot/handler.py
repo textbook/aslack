d45706593c6abff4fccfb9a04afbd00a890725c5
Jonathan Sharpe <jsharpe@pivotal.io>
2016-06-26 17:24:17 +0200

    Improve handler descriptions

--- aslack/slack_bot/handler.py
+++ aslack/slack_bot/handler.py
@@ -1,5 +1,4 @@
 """Generic message handling functionality."""
-from textwrap import dedent
 
 
 class MessageHandler:
@@ -12,6 +11,7 @@ class MessageHandler:
 
     def __init__(self, *_):
         self.text = None
+        self._description = None
 
     async def __aiter__(self):
         return self
@@ -26,7 +26,18 @@ class MessageHandler:
           :py:class:`str`: The handler's description.
 
         """
-        return dedent('\n'.join(self.__doc__.splitlines()[1:])).strip()
+        if self._description is None:
+            text = '\n'.join(self.__doc__.splitlines()[1:]).strip()
+            lines = []
+            for line in map(str.strip, text.splitlines()):
+                if line and lines:
+                    lines[-1] = ' '.join((lines[-1], line))
+                elif line:
+                    lines.append(line)
+                else:
+                    lines.append('')
+            self._description = '\n'.join(lines)
+        return self._description
 
     def matches(self, data):
         """Whether the handler should handle the current message.
--- examples/test_halliwell.py
+++ examples/test_halliwell.py
@@ -5,12 +5,16 @@ from unittest import mock
 from atmdb import TMDbClient
 import pytest
 
-from examples.halliwell import Halliwell
+from examples.halliwell import Halliwell, ActorOverlapQueryHandler
+
+
+def test_description():
+    expected = ("If you send me a message asking 'actors in' with a quoted list"
+                " of movies I will find movies featuring all of those actors.")
+    assert ActorOverlapQueryHandler(None).description() == expected
 
 
 if 'TMDB_API_TOKEN' in environ:
-
-    bot = Halliwell('abc123', '', None, tmdb_client=TMDbClient.from_env())
 
     @pytest.mark.asyncio
     async def test_halliwell_demo():
@@ -21,6 +25,7 @@ if 'TMDB_API_TOKEN' in environ:
             {'data': dumps(dict(channel='hello', type='message', text=query))},
         )
         mock_socket = mock.MagicMock()
+        bot = Halliwell('abc123', '', None, tmdb_client=TMDbClient.from_env())
         bot.socket = mock_socket
         await bot.handle_message(
             message,
